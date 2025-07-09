ab55ed96ac505bd1f25e6746fb7bbc419141cc21
wesselsmit <wrsmit00@gmail.com>
2025-07-09 23:02:24 +0200

    docs: document contact-obfuscator workings and context

--- scripts/contact-obfuscator.js
+++ scripts/contact-obfuscator.js
@@ -1,3 +1,16 @@
+/*
+ * This script obfuscates contact information until after user interaction
+ * to prevent bot scraping as much as possible whilst still keeping
+ * the content accessible to users.
+ *
+ * There are 3 scenarios/states:
+ * 1. JS is disabled -- the contact information in the HTML is formatted
+ * in a way that is human readable, but might throw off bots.
+ * 2. JS is enabled, before interaction -- contact information is replaced
+ * with "*" asterisks and a hint to let users know to click for reveal.
+ * 3. JS is enabled, after interaction -- contact information is revealed.
+*/
+
 const EMAIL_BASE64 = "d3JzbWl0MDBAZ21haWwuY29t";
 const PHONE_BASE64 = "KzMxIDY4MTcxNzU4Mg==";
 
